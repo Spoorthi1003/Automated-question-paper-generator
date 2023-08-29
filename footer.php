35478874147100641a9816a4821ab3e80cd5e6fb
Spoorthi1003 <130477860+Spoorthi1003@users.noreply.github.com>
2023-08-29 23:58:05 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<p style="margin-top:50px;">Project By: 
+    <a href="#">shabeena</span></a>, 
+  <a href="#">rakshitha</span></a>,
+  <a href="#">siri</span></a>, 
+  <a href="#">spoorthi</span></a>
+
+  
+
+</p>
